469d205971a5754d9ff19fc4a79f365488d963ea
jinwook_choi <40587435+MeongJJeong@users.noreply.github.com>
2020-02-22 15:42:30 +0900

    Update Coala_ACS.ino
    + 정보 업데이트와 프린트 부분 분리구현 (infoUpdate(), infoWrite())
    +  MsTimer2 사용 (정보 갱신 반복 분리)
    + 버튼 클릭시 딜레이 개선
    + 핀값 정의 방식 변경
    
    > 센서 오류발생시 코드 작동 안정화
    > led관련함수 생성
    > serial출력부분 개선
    > 복합정보처리 추가

--- Coala_ACS/Coala_ACS.ino
+++ Coala_ACS/Coala_ACS.ino
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>  
 #include <DHT.h>
+#include <MsTimer2.h>
 
 #define START_1 0x42  
 #define START_2 0x4d  
@@ -17,23 +18,25 @@
 #define PM2_5_PARTICLE_L   17  
 #define VERSION              26  
 #define ERROR_CODE           27  
-#define CHECKSUM             29  #define DHTTYPE DHT22 
+#define CHECKSUM             29 
 
+// pin number
 #define DHTTYPE DHT22
+#define btnPin 9
+#define dhtPin 8
+#define pmsPin 7
 
-int btnPin = 8;       // 스위치(SW) 핀 설정
-int ledPin = 10;
-int dhtPin = 4;
-int pmsPin = 7;       // LED 핀 설정
 int mode = 1;
- 
-int reading;          // SW 상태
-int previous = LOW;   // SW 이전 상태
- 
-long time = 0;        // LED가 ON/OFF 토글된 마지막 시간
-long debounce = 100;  // Debounce 타임 설정
 
-SoftwareSerial mySerial(7,6); // Arudino Uno port RX, TX  
+int ledPin[] = {0,1,2}; //ledpin number array -green, yellow, red
+
+int reading;   //btn 상태
+int previous = LOW;  //btn 이전 상태
+
+long time = 0;
+long debounce = 1000; 
+
+SoftwareSerial mySerial(7 ,6); // Arudino Uno port RX, TX  for pms7003
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(dhtPin, DHTTYPE);
 
@@ -44,109 +47,138 @@ unsigned char chrData[30];
 int PM01;  
 int PM25;  
 int PM10;  
+
+float humi;
+float temp;
       
 unsigned int GetPM_Data(unsigned char chrSrc[], byte bytHigh, byte bytLow)  
 {  
    return (chrSrc[bytHigh] << 8) + chrSrc[bytLow];  
 }  
       
-void setup(){  
-   Serial.begin(9600);  
-   Serial.println("Serial test!! ");  
-   mySerial.begin(9600);  
+void setup()
+{
 
+   Serial.begin(115200);
+   mySerial.begin(9600);
+   
    pinMode(btnPin, INPUT_PULLUP);
-
    lcd.begin();
-	lcd.backlight();
-
+   lcd.backlight();
    dht.begin();
-}  
-      
-void loop(){
 
-  reading = digitalRead(btnPin);
+   infoUpdate();
 
-  if (reading == LOW && previous == HIGH && millis() - time > debounce) {
-    if (mode == 1){
-       mode = 0;
-      Serial.println("0");
-    }  
-     
-    else{
-        mode = 1;
-        Serial.println("1!!");
-    }        
-    time = millis();
-  }
-  complexScouter(mode);
-  previous = reading;
+   MsTimer2::set(10000,infoUpdate);
+   MsTimer2::start();
+
 }
 
-void complexScouter(int mode)
+void loop() 
 {
-  //delay(2000);
-  static unsigned long count = 0;
-      if(count++ < 100000) return;
-      count = 0;
+   reading = digitalRead(btnPin);
 
-  if (mode == 0)
-  {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    if (mySerial.available())   {  
-      for(int i = 0; i < 32; i++)     {  
-         chrRecv = mySerial.read();  
-         if (chrRecv == START_2 ) {   
-            bytCount1 = 2;  
-               break;  
-         }  
-      }   
-      if (bytCount1 == 2){  
-         bytCount1 = 0;  
-         for(int i = 0; i < 30; i++){  
-            chrData[i] = mySerial.read();  
-         }   
-      
-         if ( (unsigned int) chrData[ERROR_CODE] == 0 ) {  
-            PM01  = GetPM_Data(chrData, PM1_0_ATMOSPHERE_H, PM1_0_ATMOSPHERE_L);  
-            PM25  = GetPM_Data(chrData, PM2_5_ATMOSPHERE_H, PM2_5_ATMOSPHERE_L);  
-            PM10  = GetPM_Data(chrData, PM10_ATMOSPHERE_H, PM10_ATMOSPHERE_L);  
-            lcd.print("PM1.0=");  
-            lcd.print(PM01);  
-            lcd.setCursor(0,1);
-            lcd.print("PM2.5=");  
-            lcd.print(PM25);  
-            lcd.print(" PM10=");  
-            lcd.println(PM10);  
-         }else{  
-            Serial.println("PMS7003  ERROR");  
-         }  
-      }   
-   }  
-   else{  
-      Serial.println("PMS7003 NOT available");  
-    }  
-  } else if (mode == 1){
-    lcd.clear();
+   if (reading == LOW && previous == HIGH && millis() - time > debounce)
+   {
+      if (mode == 1)
+      {
+         mode = 0;
+         Serial.println("mode 0");
+      } else{
 
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+         mode = 1;
+         Serial.println("mode 1");
+      }
+      time = millis();
+      infoWrite(mode);
+   }
+   previous = reading;
+}
 
-    if (isnan(h) || isnan(t)) {
+void infoWrite (int mode)
+{
+
+   lcd.clear();
+
+   if (mode == 0)    // dht info print
+   {
+      lcd.setCursor(0,0); 
+      lcd.print("Temp.: "); 
+      lcd.print(temp); 
+      lcd.print(" C");     
+      lcd.setCursor(0,1);
+      lcd.print("Humi.: ");
+      lcd.print(humi);
+      lcd.print(" %"); 
+   } else if (mode == 1)   //pms info print
+   {
+      lcd.setCursor(0,0);
+      lcd.print("PM1.0=");  
+      lcd.print(PM01);  
+      lcd.setCursor(0,1);
+      lcd.print("PM2.5=");  
+      lcd.print(PM25);  
+      lcd.setCursor(8,1);
+      lcd.print(" PM10=");  
+      lcd.println(PM10);
+   }
+   
+}
+
+void infoUpdate () 
+{
+   // dht update
+   humi = dht.readHumidity();
+   temp = dht.readTemperature();
+
+   if (isnan(humi) || isnan(temp)) {
       Serial.println(F("Failed to read from DHT sensor!"));
       lcd.setCursor(0,0);
       lcd.print("Failed to read from DHT sensor!");
-      return;
-    }
 
-    lcd.setCursor(0,0); // Sets the location at which subsequent text written to the LCD will be displayed
-    lcd.print("Temp.: "); // Prints string "Temp." on the LCD
-    lcd.print(t); // Prints the temperature value from the sensor
-    lcd.print(" C");     
-    lcd.setCursor(0,1);
-    lcd.print("Humi.: ");
-    lcd.print(h);
-    lcd.print(" %"); 
+      infoUpdate();
+      return;
    }
+
+   // pms update
+   if (mySerial.available())
+   {
+      for (int i = 0; i < 32; i++)
+      {
+         chrRecv = mySerial.read();
+         if (chrRecv == START_2)
+         {
+            bytCount1 = 2;
+            break;
+         }
+      }
+
+      if (bytCount1 == 2) 
+      {
+         bytCount1 = 0;
+         for (int i = 0; i < 30; i++)
+         {
+            chrData[i] = mySerial.read();
+         }
+
+         if ((unsigned int) chrData[ERROR_CODE] == 0)
+         {
+            PM01  = GetPM_Data(chrData, PM1_0_ATMOSPHERE_H, PM1_0_ATMOSPHERE_L);  
+            PM25  = GetPM_Data(chrData, PM2_5_ATMOSPHERE_H, PM2_5_ATMOSPHERE_L);  
+            PM10  = GetPM_Data(chrData, PM10_ATMOSPHERE_H, PM10_ATMOSPHERE_L); 
+         } else {
+            Serial.println("PMS7003 ERROR");
+            infoUpdate();
+         }
+      }
+   } else
+   {
+      Serial.println("PMS7003 NOT available");  
+      infoUpdate();
+   }
+   
+
+   //lcd update
+   infoWrite(mode);
+
 }
