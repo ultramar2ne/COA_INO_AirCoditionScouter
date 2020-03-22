63c67f15dbd84c77f64964d3977b7eb919252423
jinwook_choi <40587435+MeongJJeong@users.noreply.github.com>
2020-03-22 16:09:18 +0900

    Update dht22.ino
    샘플코드 없데이트

--- SampleCode/DHT22/dht22.ino
+++ SampleCode/DHT22/dht22.ino
@@ -1,1 +1,31 @@
+#include <DHT.h>
 
+#define dhtPin 8
+#define DHTTYPE DHT22
+
+DHT dht(dhtPin, DHTTYPE);
+float humi;
+float temp;
+
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop()
+{
+    delay(3000);
+    humi = dht.readHumidity();
+    temp = dht.readTemperature();
+
+   if (isnan(humi) || isnan(temp)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+   }
+   Serial.print("Temp : ");
+   Serial.print(temp);
+   Serial.print(" C , humi : ");
+   Serial.print(humi);
+   Serial.println(" %");
+}
