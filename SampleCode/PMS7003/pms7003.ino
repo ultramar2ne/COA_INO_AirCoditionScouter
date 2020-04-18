06c7f99d115d1b0fac717651ad539f756f7fb190
jinwook_choi <40587435+MeongJJeong@users.noreply.github.com>
2020-04-19 02:13:16 +0900

    Update pms7003.ino

--- SampleCode/PMS7003/pms7003.ino
+++ SampleCode/PMS7003/pms7003.ino
@@ -1,51 +1,45 @@
+// from: https://github.com/fu-hsi/PMS.git
+#include "PMS.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(7,6); // Arudino Uno port RX, TX
+SoftwareSerial Serial1(7 , 6);
+PMS pms(Serial);
+PMS::DATA data;
 
-void setup() {
-  // for debuging 
-  Serial.begin(9600);  
-  
-  // Use software serial
-  mySerial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);   // GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
+  Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
+  pms.passiveMode();    // Switch to passive mode
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  static int CheckFirst=0;
-  static int pm_add[3][5]={0,};
-  static int pm_old[3]={0,};
-  int chksum=0,res=0;;
-  unsigned char pms[32]={0,};
-  
-  
-  if(mySerial.available()>=32){
+void loop()
+{
+  Serial1.println("Waking up, wait 30 seconds for stable readings...");
+  pms.wakeUp();
+  delay(30000);
 
-    for(int j=0; j<32 ; j++){
-      pms[j]=mySerial.read();
-      if(j<30)
-        chksum+=pms[j];
-    }
+  Serial1.println("Send read request...");
+  pms.requestRead();
 
-    if(pms[30] != (unsigned char)(chksum>>8) 
-        || pms[31]!= (unsigned char)(chksum) ){
+  Serial1.println("Reading data...");
+  if (pms.readUntil(data))
+  {
+    Serial1.print("PM 1.0 (ug/m3): ");
+    Serial1.println(data.PM_AE_UG_1_0);
 
-      return res;
-    }
-    if(pms[0]!=0x42 || pms[1]!=0x4d )
-      return res;
+    Serial1.print("PM 2.5 (ug/m3): ");
+    Serial1.println(data.PM_AE_UG_2_5);
 
-  Serial.print("Dust raw data debugging :  ");
-  Serial.print("1.0ug/m3:");
-  Serial.print(pms[10]);
-  Serial.print(pms[11]);
-  Serial.print("  ");
-  Serial.print("2.5ug/m3:");
-  Serial.print(pms[12]);
-  Serial.print(pms[13]);
-  Serial.print("  ");
-  Serial.print("2.5ug/m3:");
-  Serial.print(pms[14]);
-  Serial.println(pms[15]);
-  } 
+    Serial1.print("PM 10.0 (ug/m3): ");
+    Serial1.println(data.PM_AE_UG_10_0);
+  }
+  else
+  {
+    Serial1.println("No data.");
+  }
+
+  Serial1.println("Going to sleep for 60 seconds.");
+  pms.sleep();
+  delay(60000);
 }
